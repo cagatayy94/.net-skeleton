54108a4edf6a53d30aff67664abdfae255166e04
Mustafa Çağatay Yılmaz <mustafa.cagatay.yilmaz@gmail.com>
2021-02-13 22:16:04 +0300

    Created result types

---
+++ Core/Utilities/Results/ErrorDataResult.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Core.Utilities.Results;
+
+namespace Core.Utilities.Results
+{
+    public class ErrorDataResult<T> : DataResult<T>
+    {
+        public ErrorDataResult(T data, string message) : base(data, false, message)
+        {
+
+        }
+
+        public ErrorDataResult(T data) : base(data, true)
+        {
+
+        }
+
+        public ErrorDataResult(string message) : base(default, false, message)
+        {
+
+        }
+
+
+        public ErrorDataResult() : base(default, false)
+        {
+
+        }
+    }
+}
