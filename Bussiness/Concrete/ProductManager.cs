3b50800e9e56e27e1b89ffa2168a618493897f0a
Mustafa Çağatay Yılmaz <mustafa.cagatay.yilmaz@gmail.com>
2021-02-13 23:10:13 +0300

    Updated business layer for result classes implementation

--- Bussiness/Concrete/ProductManager.cs
+++ Bussiness/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Bussiness.Abstract;
 using Bussiness.Constants;
 using Core.Utilities.Results;
@@ -30,35 +31,39 @@ namespace Bussiness.Concrete
             return new SuccessResult(Messages.ProductAdded);
         }
 
-        public List<Product> GetAll()
+        public IDataResult<List<Product>> GetAll()
         {
+            if(DateTime.Now.Hour == 22)
+            {
+                return new ErrorDataResult<List<Product>>(Messages.MaintenenceTime);
+            }
             //rules
-            return _productDal.GetAll();
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(), Messages.ProductsListed);
         }
 
-        public List<Product> GetAllByCategoryId(int categoryId)
+        public IDataResult<List<Product>> GetAllByCategoryId(int categoryId)
         {
-            return _productDal.GetAll(p => p.CategoryId == categoryId);
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.CategoryId == categoryId));
         }
 
-        public List<Product> GetAllByUnitPrice(decimal min, decimal max)
+        public IDataResult<List<Product>> GetAllByUnitPrice(decimal min, decimal max)
         {
-            return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max));
         }
 
-        public List<Product> GetAllByUnitsInStock(int min, int max)
+        public IDataResult<List<Product>> GetAllByUnitsInStock(int min, int max)
         {
-            return _productDal.GetAll(p => p.UnitsInStock >= min && p.UnitsInStock <= max);
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.UnitsInStock >= min && p.UnitsInStock <= max));
         }
 
-        public Product GetById(int productId)
+        public IDataResult<Product> GetById(int productId)
         {
-            return _productDal.Get(p => p.ProductId == productId);
+            return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
         }
 
-        public List<ProductDetailDto> GetProductDetails()
+        public IDataResult<List<ProductDetailDto>> GetProductDetails()
         {
-            return _productDal.GetProductDetails();
+            return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetails());
         }
     }
 }
--- Bussiness/Constants/Messages.cs
+++ Bussiness/Constants/Messages.cs
@@ -5,5 +5,7 @@ namespace Bussiness.Constants
     {
         public static string ProductAdded = "Ürün eklendi";
         public static string ProductNameInvalid = "Ürün İsmi geçersiz";
+        public static string MaintenenceTime = "Bakım zamanı lütfen daha sonra tekrar deneyin";
+        public static string ProductsListed = "Ürünler listelendi";
     }
 }
