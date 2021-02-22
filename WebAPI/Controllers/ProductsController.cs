ccc0e240cd2ef4482242e62fef832824dbb17ed5
Mustafa Çağatay Yılmaz <mustafa.cagatay.yilmaz@gmail.com>
2021-02-22 21:11:53 +0300

    Added web API

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bussiness.Abstract;
+using Bussiness.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+            
+        [HttpGet]
+        public List<Product> Get()
+        {
+            return _productService.GetAll().Data;
+        }
+    }
+}
