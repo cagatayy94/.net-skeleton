e0b9fee7cb4bd223bcbb71b9f6f14921472cfcbe
mustafa cagatay yılmaz <cagatay.yilmaz@ozan.com>
2021-02-24 22:12:07 +0300

    Implemented autofac for depencency injection

---
+++ Bussiness/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Autofac;
+using Bussiness.Abstract;
+using Bussiness.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
+namespace Bussiness.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
+            builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
+        }
+    }
+}
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -36,8 +36,7 @@ namespace WebAPI
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI", Version = "v1" });
             });
-            services.AddSingleton<IProductService, ProductManager>();
-            services.AddSingleton<IProductDal, EfProductDal>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
