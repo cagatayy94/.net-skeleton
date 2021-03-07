7e6d7b402fd8ee4ff78affbb6cc7477f6ab20e26
mustafa cagatay yılmaz <cagatay.yilmaz@ozan.com>
2021-03-08 02:14:49 +0300

    Added cache aspect

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -5,6 +5,7 @@ using Business.Abstract;
 using Business.BusinessAspect.Autofac;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Caching;
 using Core.Aspects.Autofac.Transaction;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
@@ -30,6 +31,7 @@ namespace Business.Concrete
 
         [SecuredOperation("product.add,admin")]
         [ValidationAspect(typeof(ProductValidator))]
+        [CacheRemoveAspect("IProductService.Get")]
         public IResult Add(Product product)
         {
             IResult result = BusinessRules.Run (CheckIfProductCountOfCategoryCorrect(product.CategoryId),
@@ -46,6 +48,7 @@ namespace Business.Concrete
             return new SuccessResult(Messages.ProductAdded);            
         }
 
+        [CacheAspect]
         public IDataResult<List<Product>> GetAll()
         {
             if(DateTime.Now.Hour == 10)
@@ -71,6 +74,7 @@ namespace Business.Concrete
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.UnitsInStock >= min && p.UnitsInStock <= max));
         }
 
+        [CacheAspect]
         public IDataResult<Product> GetById(int productId)
         {
             return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
