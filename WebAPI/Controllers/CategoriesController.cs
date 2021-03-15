7ffa11264177e571f55281004c7caa214906a76e
mustafa cagatay yılmaz <cagatay.yilmaz@ozan.com>
2021-03-15 22:18:13 +0300

    Added category controller

---
+++ WebAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController:ControllerBase
+    {
+        ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            Thread.Sleep(1000);
+            var result = _categoryService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+    }
+}
